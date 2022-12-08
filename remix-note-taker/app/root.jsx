0a280a5799f323504cf3c1a7ba8fe7eb1fbec808
Shawn Johnson <johnson.sg@gmail.com>
2022-12-08 18:12:42 -0500

    Notes Data added

--- remix-note-taker/app/root.jsx
+++ remix-note-taker/app/root.jsx
@@ -4,35 +4,39 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+  ScrollRestoration
+} from '@remix-run/react'
 
 // Global Style = main.css
 import styles from '~/styles/main.css'
+import MainNavigation from '~/components/Navigtion/MainNavigation'
 
 export const meta = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1'
+})
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
       </head>
       <body>
+        <header>
+          <MainNavigation />
+        </header>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
 
 export function links() {
-  return [{ rel: 'stylesheet', href: styles}]
+  return [{ rel: 'stylesheet', href: styles }]
 }
--- remix-note-taker/app/routes/notes.jsx
+++ remix-note-taker/app/routes/notes.jsx
@@ -1,14 +1,36 @@
-import NewNote, {links as newNoteLinks } from '~/components/Notes/NewNote'
+import { redirect } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import NewNote, { links as newNoteLinks } from '~/components/Notes/NewNote'
+import NoteList, { links as noteListLinks} from '~/components/Notes/NoteList'
+import { getStoredNotes, storeNotes } from '~/data/notes'
 
 export default function NotesPage() {
+  const notes = useLoaderData()
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   )
 }
 
+export async function loader() {
+  const notes = await getStoredNotes()
+  return notes
+}
+
+export async function action({ request }) {
+  const formData = await request.formData()
+  const noteData = Object.fromEntries(formData)
+  // Add Validation...
+  const existingNotes = await getStoredNotes()
+  noteData.id = new Date().toISOString()
+  const updatedNotes = existingNotes.concat(noteData)
+  await storeNotes(updatedNotes)
+  return redirect('/notes')
+}
+
 export function links() {
   // Surfacing Styles in Remix.
-  return [...newNoteLinks()]
+  return [...newNoteLinks(), ...noteListLinks()]
 }
